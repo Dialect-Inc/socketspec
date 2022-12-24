5c4f9fd9092e072679d1d40760532d477189b4fb
Leon Si <leon@leondreamed.com>
2022-12-24 05:53:43 -0500

    feat: update exports

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
 export type {
+	ClientSocket,
+	ServerSocket,
 	SocketEventDefinition,
 	SocketEventDefinitionData,
+	// Exporting `SocketHandler` is needed to avoid the following error:
+	// "The inferred type of 'myHandler' cannot be named without a reference to '~/node_modules/@dialect-inc/socket/src/types/socket.js'."
+	SocketEventHandler,
+	SocketEventHandlerContext,
+	SocketEventHandlerDefinition,
 	SocketHandlerResponse
 } from './types/socket.js'
+export { defineSocketEventHandler } from './utils/event-handler.js'
 export { createUseSocketEventHook } from './utils/hooks.js'
 export {
 	createClientSocketMessageHandler,
